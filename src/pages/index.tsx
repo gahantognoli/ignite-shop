e3647ae7b0e2c009d2992679b8fe1c94ad7cd321
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2024-12-26 22:15:57 -0300

    Estrutura visual do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,51 @@
+import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
+
+import "keen-slider/keen-slider.min.css";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 80,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 2</strong>
+          <span>R$ 120,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
